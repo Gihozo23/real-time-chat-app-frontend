c983af57d977c32d97ea7d71a0d052cd496204fc
Gihozo23 <christellegihozo23@gmail.com>
2024-12-31 15:13:48 +0200

    feat: integrate the InputComponent in the Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Chat from "../assets/chat";
 import React from "react";
 import Profile from "../assets/Profile";
+import InputComponent from "./InputComponent";
 export default function Header() {
   return (
     <>
@@ -8,7 +9,12 @@ export default function Header() {
         <Chat className="text-link h-6 w-6 md:h-10 md:w-12">Yappanese</Chat>
         <Profile className="text-link h-6 w-6 md:h-10 md:w-12" />
       </header>
-      <div className="bg-background h-screen pt-2 ml-64 relative top-0 rounded-lg z-100"></div>
+      <div className="bg-background h-screen pt-2 ml-64 relative top-0 rounded-lg z-100 flex-col justify-around"></div>
+      <div className="ml-64 ">
+        <InputComponent labelName="Message" action="Send" />
+        <InputComponent labelName="Room" action="Join" />
+      </div>
+
       {/* <hr className="border-2 border-gray-600 my-6" /> */}
     </>
   );
